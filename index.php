b8fea5f55e77891d98d2171baab092d076dc00b6
omar906 <omar3675@hotmail.com>
2022-11-02 17:44:41 -0500

    se agrga el proyecto

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>estilos bootstrapp></title>
+    <link rel="stylesheet" href="./css/index.css" >
+
+</head>
+<body>
+    <nav>
+        <h1>MUSICA 80 </h1>
+            <ul class="nave">
+                <li><a href="#">inicio</a> </li>
+                <li><a href="#">sobre nostros </a> </li>
+                <li><a href="#">block</a></li>
+                <li><a href="#">contactos</a></li>
+                <li><a href="#">registro</a></li>
+                
+      </ul>
+    </nav>
+     
+    <header class="uno">
+       <img src="./imagen/blog_1.jpg">
+
+
+    <div class="imagen">
+        <img src="portada.jpeg">
+      
+    </div>
+    <p class="comen">Es estos dias se viralizo nuevos versiones de temas que,
+       escuchados con auriculares, suena de una manera muy de difente a la original.
+      ¿aun no escuchaste? sorprendente  </p>
+
+
+    </header>
+    <section class="music">
+         <img src="">
+         <img src="">
+         <img src="">
+    </section>
+    
+     <div class="pregunta">
+      <h3>¿de que se trata?</h3>
+      <p> se trata de una tecnica a los principios de los años 70
+        en inglaterra, que cumplian una funcion de darle el sonido un caracteristica esferica
+      </p>
+      <p>a grandes rasgos, a partir de un complicado trabajo que tiene base matematicas,
+        se trabaja la fase del sonido que permite que el celebro distinga de donde viene la informacion 
+
+      </p>
+     </div> 
+             <div class="actual">
+              <div class="actual-tex">
+                <h3>Trabajo actuales</h3>
+                <p>musica ab actalmente es trabajando en un albun enteramente en 
+                  sonido 360" con artistas argentinos y extranjeros como proyecto que
+                  Gomez, Casas,
+                </p>
+              </div>
+             </div>
+   
+    
+</body>
+</html>
